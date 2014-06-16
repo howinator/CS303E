e7583fb9bcc30afa07ee9aeaf17b27a51b8a554e
HowieBenefiel <hben592@gmail.com>
2014-06-15 23:13:21 -0500

    First assignment. It just makes a 'X' out of whitespace.

---
+++ Pattern.py
@@ -0,0 +1,48 @@
+# File: Pattern.py
+
+# Description: Prints an 'X' onf whitespace to command line
+
+# Student Name: Paul Benefiel
+
+# Student UT EID: phb337
+
+# Course Name: CS 303E
+
+# Unique Number: 90110
+
+# Date Created: June 15th, 2014 
+
+# Date Last Modified: June 15th, 2014 
+
+############################################################################
+def main():
+    # 2 Blank lines at the top of the figure
+    print('')
+    print('')
+    # Printing of the actual figure
+    print('     *************************')
+    print('     *  *******************  *')
+    print('     **  *****************  **')
+    print('     ***  ***************  ***')
+    print('     ****  *************  ****')
+    print('     *****  ***********  *****')
+    print('     ******  *********  ******')
+    print('     *******  *******  *******')
+    print('     ********  *****  ********')
+    print('     *********  ***  *********')
+    print('     **********  +  **********')
+    print('     *********  ***  *********')
+    print('     ********  *****  ********')
+    print('     *******  *******  *******')
+    print('     ******  *********  ******')
+    print('     *****  ***********  *****')
+    print('     ****  *************  ****')
+    print('     ***  ***************  ***')
+    print('     **  *****************  **')
+    print('     *  *******************  *')
+    print('     *************************')
+    print('')
+    print('')
+
+# Now we call the function
+main()
